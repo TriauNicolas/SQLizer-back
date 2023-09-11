4e4146ba1f46f44ae3134876fd105495a4604720
gabrielseron <gabriel.seron.pro@gmail.com>
2023-09-11 15:17:58 +0200

    Update: User join workgroup room

--- src/core/sockets.ts
+++ src/core/sockets.ts
@@ -6,7 +6,7 @@ let io;
 export async function initSocket(httpServer) {
     (io as Server) = new Server(httpServer, { path: '/sqlizer/', cors: { origin: '*' } });
 
-    io.on('connection', async (socket: Socket) => {
+    io.on('connection', async (socket: Socket & { request: {_query: { room: string } } } ) => {
         socket.join('room1');
         const token = Array.isArray(socket.request.headers.bearertoken) ? socket.request.headers.bearertoken[0] : socket.request.headers.bearertoken;
         if (token === null || typeof(token) === 'undefined') {
@@ -14,8 +14,10 @@ export async function initSocket(httpServer) {
         } else {
             try {
                 const user = await getUserFromToken(token);
-                socket.join(user.id);
+                socket.join([user.id, socket.request._query.room]);
+                io.to(socket.request._query.room).emit('lorem');
                 console.log(user.id + ' Connected with socketId : ' + socket.id);
+                // to send an event to a specific user, you must do getSocketIo().to(userId).emit('event', data)
             } catch (error) {
                 console.log(error.message);
             }
@@ -23,6 +25,6 @@ export async function initSocket(httpServer) {
     });
 }
 
-export function getSocketIo() {
+export function getSocketIo(): Server {
 	return io as Server;
 }
