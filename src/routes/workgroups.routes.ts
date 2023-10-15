ced1bfc35e58ac4122fa485b5185b3b5eae28b9c
gabrielseron <gabriel.seron.pro@gmail.com>
2023-10-15 14:04:10 +0200

    Feat: Add multiple route for update rights

--- src/routes/workgroups.routes.ts
+++ src/routes/workgroups.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { getWorkgroupsController, createWorkgroupController, addUserToWorkgroupController, deleteWorkgroupController, updateUserRightController, removeUserOfWorkgroupController, getWorkgroupsDatasController } from '../controllers/workgroups.controllers';
+import { getWorkgroupsController, createWorkgroupController, addUserToWorkgroupController, deleteWorkgroupController, updateUserRightController, removeUserOfWorkgroupController, getWorkgroupsDatasController, updateUserCreateRightController, updateUserDeleteRightController, updateUserUpdateRightController } from '../controllers/workgroups.controllers';
 
 const route: Router = Router();
 
@@ -8,6 +8,9 @@ route.post('/createWorkgroup', createWorkgroupController);
 route.put('/addUserToWorkgroup', addUserToWorkgroupController);
 route.delete('/deleteWorkgroup', deleteWorkgroupController);
 route.put('/updateUserRight', updateUserRightController);
+route.put('/updateUserCreateRight', updateUserCreateRightController);
+route.put('/updateUserUpdateRight', updateUserUpdateRightController);
+route.put('/updateUserDeleteRight', updateUserDeleteRightController);
 route.delete('/removeUserOfWorkgroup', removeUserOfWorkgroupController);
 route.get('/getUserWorkgroupsDatas', getWorkgroupsDatasController);
 
